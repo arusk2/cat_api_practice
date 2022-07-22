434e9e144c55151046f6cc817c48282d82cbdbce
andrew <aruskampwhite@gmail.com>
2022-07-22 10:01:39 -0700

    added requirements and started calculator application

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+class Calculator:
+    def __init__(self):
+        pass
+    def add(self, first, second):
+        return first + second;
+    def subtract(self, first, second):
+        pass
+    def multiply(self, first, second):
+        pass
+    def divide(self, first, second):
+        pass
+    def mod(self, first, second):
+        pass
