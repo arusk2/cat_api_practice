619d556c2c1fd33e9229159c1dea0450ac72884c
andrew <aruskampwhite@gmail.com>
2022-07-26 12:06:05 -0700

    starting second part of api tutorial: accessing data from disk

--- api_final.py
+++ api_final.py
@@ -5,25 +5,35 @@ import Calculator
 app = Flask(__name__)
 api = Api(app)
 
-# Need to create a parser for our arguments
-parser = reqparse.RequestParser()
-parser.add_argument('first')
-parser.add_argument('second')
-
 class HelloWorld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {'hello': 'world!'}
 
 class CalculatorAdd(Resource):
     def get(self, first, second):
+        # Need to create a parser for our arguments
+        parser = reqparse.RequestParser()
+        parser.add_argument('first')
+        parser.add_argument('second')
+
         calc = Calculator.Calculator()
         result = calc.add(first, second)
         return result, 200 #200 is the OK code
 
 class CalculatorSubtract(Resource):
     def get(self, first, second):
+        # Need to create a parser for our arguments
+        parser = reqparse.RequestParser()
+        parser.add_argument('first')
+        parser.add_argument('second')
+
         calc = Calculator.Calculator()
         return calc.subtract(first, second), 200
+
+class Todo(Resource):
+
+class TodoList(Resource):
+
 
 # adding resource name and the path to find the resource
 api.add_resource(HelloWorld, '/')
