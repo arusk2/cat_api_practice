49b911da6b27dc7dd2d5eee52b88a202da9f6d7f
andrew <aruskampwhite@gmail.com>
2022-09-15 10:14:21 -0700

    fixed indents

--- api_final.py
+++ api_final.py
@@ -80,31 +80,32 @@ class Cat(Document):
     major = StringField(max_length=100)
 
 
-   """ Notes for about MongoEngine (This is an implementation specific to mongoengine
-    we need to define a page, Which is where we store items with mongoengine
-    All items with this class, Cat, will save on the same page in Mongo DB, "Cat"
-    It must inherent the mongoengine class Document
-    StringField and IntField are requirements by MongoEngine so we can store data effectively in the db
+""" Notes for about MongoEngine (This is an implementation specific to mongoengine
+we need to define a page, Which is where we store items with mongoengine
+All items with this class, Cat, will save on the same page in Mongo DB, "Cat"
+It must inherent the mongoengine class Document
+StringField and IntField are requirements by MongoEngine so we can store data effectively in the db
 
-    Adding new items: var = PageName(field1='', field2='', fieldx='') 
-    where PageName is the defined page as given. (in our case, its Cat) and each value in args matches type.
-    We simply create a Cat object by saying: new_cat = Cat(name='', age='', major=''), just like we'd create a class object 
-    normally. This adds a new entry to the 'Cat' Page on the database at the MongoDB URI we've previously established.
-    Saving the data:
-        var.save() 
-    """
+Adding new items: var = PageName(field1='', field2='', fieldx='') 
+where PageName is the defined page as given. (in our case, its Cat) and each value in args matches type.
+We simply create a Cat object by saying: new_cat = Cat(name='', age='', major=''), just like we'd create a class object 
+normally. This adds a new entry to the 'Cat' Page on the database at the MongoDB URI we've previously established.
+Saving the data:
+    var.save() 
+"""
 
-    """ We want to be able to do CRUD operations to the database. (Create, Read, Update, Delete). These are done with
-    our REST API. why?
-    We don't want to transfer the entire contents of the database to our code to add an entry, or to read entries.
-    That's hugely wasteful. Instead, we want to include only data that the database needs to use (for updating) 
-    or the user needs (for reading). This is our "State" (the S in REST). The state is usually a subset of database, 
-    and is treated as a "REpresentation" of database itself. We then make changes to the "State" and "Transfer" it back 
-    that to the server. (RE S T... see?)
-    Example: The Create Operation: The State starts empty, because the new entry doesn't exist in our 
-        Representation of the DB. Then, we modify the State to have a new record, Transfer that back to the original
-        Representation & the DB handles adding that record into its larger pieces.
-    """
+""" 
+We want to be able to do CRUD operations to the database. (Create, Read, Update, Delete). These are done with
+our REST API. why?
+We don't want to transfer the entire contents of the database to our code to add an entry, or to read entries.
+That's hugely wasteful. Instead, we want to include only data that the database needs to use (for updating) 
+or the user needs (for reading). This is our "State" (the S in REST). The state is usually a subset of database, 
+and is treated as a "REpresentation" of database itself. We then make changes to the "State" and "Transfer" it back 
+that to the server. (RE S T... see?)
+Example: The Create Operation: The State starts empty, because the new entry doesn't exist in our 
+    Representation of the DB. Then, we modify the State to have a new record, Transfer that back to the original
+    Representation & the DB handles adding that record into its larger pieces.
+"""
 
 
 # Create
@@ -121,16 +122,19 @@ class AddCat(Resource):
 
         # Third, we must connect to our resource, the DB. We're utilizing globals established above for convenience.
         connect(alias=DB, host=MONGO_URI)
-        # Using Mongo Engine, we can create a Cat object. This can then be saved to the active database
+        # Foruth, using Mongo Engine, we can create a Cat object. This can then be saved to the active database
         # DB is our global variable for the specific page name. This technically returns a database object,
         # but it isn't used specifically, so we won't save it. Once the connection is established, we can create the
         # Cat Object and the Cat.save() function saves the new object to the database we connected to with DB
         new_cat = Cat(name=name, age=age, major=major)
         new_cat.save()
+        # Fifth, disconnect
         disconnect(alias=DB)
 
-        # Here we would decide what to return. Since this is a toy case, we will always return 200.
-        # Improvements: If auth fails, should we return some other HTML code? what about if the record exists already?
+        # Here we would decide what to return. Since this is a toy case, we will always return 200. We don't want
+        # to return any data from the database.
+        # Improvements: If auth fails, should we return some other HTML code?
+        # What about if the record exists already or the user doesn't use the correct fields?
         return 200
 
 
